768a73fdb380e29edbad40ff11c02f9fc16788c2
sunil <msunilhyd@gmail.com>
2023-06-14 16:08:12 -0700

    Generic Commit

---
+++ next/ninjalist/src/pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Never outshine the master</p>
+      <p>Do not put too much trust in friends</p>
+    </div>
+  )
+}
+
+export default About;
---
+++ next/ninjalist/src/pages/contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <p>This is the contact page</p>
+      <p>Call us on 595 484 2331</p>
+    </div>
+  )
+}
+
+export default Contact;
